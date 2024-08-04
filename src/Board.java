08a793ad0268eb14642e1fa340d0bafab9d1815c
PatS7 <pmwine@outlook.com>
2024-08-04 19:42:50 +1200

    One more test to check the connection between eclipse and github is working (had lots of issues)

--- src/Board.java
+++ src/Board.java
@@ -13,7 +13,7 @@ public class Board {
 	public Board() {
 		
 	 //TODO: initialise the cells array using ROWS and COLS constants 
-		//ONE MORE TEST
+		//FINAL TEST BEFORE BEGINNING
 
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
