fa6a103b4ec0b1ad10afc9add9b3425aff2c2f26
PatS7 <pmwine@outlook.com>
2024-08-06 14:16:04 +1200

    loops added to hasWon method to check for if player wins in a column and opposite diagonal.

--- src/Board.java
+++ src/Board.java
@@ -41,22 +41,32 @@ public class Board {
 	/** Return true if the current player "thePlayer" has won after making their move  */
 	public boolean hasWon(Player thePlayer, int playerRow, int playerCol) {
 		 // check if player has 3-in-that-row
-		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
+		if(cells[playerRow][0].content == thePlayer 
+			&& cells[playerRow][1].content == thePlayer 
+			&& cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
+		 //check if player has won with with 3 in a column
+		
+		if (cells[0][playerCol].content == thePlayer && 
+	            cells[1][playerCol].content == thePlayer && 
+	            cells[2][playerCol].content == thePlayer)
+	            return true;
 		
 		
-		
-		 // 3-in-the-diagonal
-		if( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
+		 // 3-in-the-diagonal (top left to bottom right)
+		if( cells[0][0].content == thePlayer && 
+			cells[1][1].content == thePlayer && 
+			cells[2][2].content == thePlayer)
 			return true;
 		 
 		
-		// TODO: Check the diagonal in the other direction
+		//3 in a diagonal in opposite direction (top right to bottom left)
+		if (cells[0][2].content == thePlayer && 
+	        cells[1][1].content == thePlayer && 
+	        cells[2][0].content == thePlayer)
+	        return true;
 		
-
 		
 		//no winner, keep playing
 		return false;
