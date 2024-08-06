23c5e646bb883a1794531fdc543872b99179cc51
PatS7 <pmwine@outlook.com>
2024-08-06 14:27:03 +1200

    created enumeration for GameStates

--- src/GameMain.java
+++ src/GameMain.java
@@ -26,8 +26,11 @@ public class GameMain extends JPanel implements MouseListener{
 	// the game board 
 	private Board board;
 	 	 
-	//TODO: create the enumeration for the variable below (GameState currentState)
-	//HINT all of the states you require are shown in the code within GameMain
+	//enumeration for different states(results) of the game
+	private enum GameState{
+		Playing, Draw, Cross_won, Nought_won
+	}
+	
 	private GameState currentState; 
 	
 	// the current player
